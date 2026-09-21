7b3e5b4670adfbf343d066917cc2f18e875e8817
Anna Benamati <125204414+AnnaBenamati@users.noreply.github.com>
2026-09-21 15:44:32 +0200

    v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='registro-spese-html-v2';
+const CACHE='registro-spese-html-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
